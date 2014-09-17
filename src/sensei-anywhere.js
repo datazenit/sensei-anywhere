347ff67108d3bea34a429ee1f79fb5dd09d0cfaf
Lauris <lauris@discuss.lv>
2014-09-17 22:18:22 +0300

    Revert "Add better match function."
    This reverts commit a243fba8e0caa36e81d36a60dda187cd5f880be0.

--- src/sensei-anywhere.js
+++ src/sensei-anywhere.js
@@ -1,7 +1,20 @@
 (function ($) {
 
     function match(search, text) {
-        return search.toUpperCase().lastIndexOf(text.toUpperCase(), 0) === 0;
+        search = search.toUpperCase();
+        text = text.toUpperCase();
+
+        var j = -1; // remembers position of last found character
+
+        // consider each search character one at a time
+        for (var i = 0; i < search.length; i++) {
+            var l = search[i];
+            if (l == ' ') continue;     // ignore spaces
+
+            j = text.indexOf(l, j+1);     // search for character & update position
+            if (j == -1) return false;  // if it's not found, exclude this item
+        }
+        return true;
     }
 
     $.anywhere = function (data, shortcuts) {
